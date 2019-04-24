e1524a7f05876fa05ea05a710c36576d899dd954
ini-obong <francisifon@outlook.com>
2019-04-23 22:36:30 -0400

    new folders

---
+++ src/Homework.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author franc
+ */
+import javax.swing.JOptionPane;
+public class Homework {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "Programming in Java is easy!",
+                "I learned something cool this Monday!",
+                JOptionPane.INFORMATION_MESSAGE);     
+    System.out.println("Hello Francis!");
+    System.out.println("");
+    System.out.println("3 * 7 + 11 - 10 = " + (3 * 7 + 11 - 10));
+    
+    
+    }
+    
+}
